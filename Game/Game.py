484615621dc8018c1d70c84aab6b9900b147a256
tjddus0403 <70639589+tjddus0403@users.noreply.github.com>
2020-11-18 00:03:44 +0900

    Add files via upload

---
+++ Game/Game.py
@@ -0,0 +1,20 @@
+from match import*
+from original import*
+from gallery import*
+while True:
+   print("MODE")
+   print("1. Original \n 2. Matching \n 3. Gallery \n 4. Turn off")
+   choice=int(input("Select the Mode : "))
+   if choice==1:
+      memory_key()
+      continue
+   elif choice==2:
+      memory_to()
+      continue
+   elif choice==3:
+      gallery_mode_exe()
+      continue
+   elif choice==4:
+      break
+   else:
+      continue
