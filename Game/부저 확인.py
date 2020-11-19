065bfe7d3738ad0f1856bec5e0d6d481b1800035
tjddus0403 <70639589+tjddus0403@users.noreply.github.com>
2020-11-19 20:44:45 +0900

    Update 부저 확인.py

--- Game/부저 확인.py
+++ Game/부저 확인.py
@@ -1,13 +1,19 @@
-import RPi.GPIO as GPIO
-from time import sleep
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-buzzer=23
-GPIO.setup(buzzer,GPIO.OUT)
-while True:
-   GPIO.output(buzzer,GPIO.HIGH)
-   print("Beep")
-   sleep(0.5)
-   GPIO.output(buzzer, GPIO.LOW)
-   print("No Beep")
-   sleep(0.5)
+import RPi.GPIO as GPIO 
+import time GPIO.setmode(GPIO.BCM) 
+gpio_pin = 12 
+scale = [ 261, 294, 329, 349, 392, 440, 493, 523 ] 
+GPIO.setup(gpio_pin, GPIO.OUT) 
+list = [4, 4, 5, 5, 4, 4, 2, 4, 4, 2, 2, 1] 
+try: 
+   p = GPIO.PWM(gpio_pin, 100) 
+   p.start(100) 
+   p.ChangeDutyCycle(90) 
+   for i in range(12): 
+      p.ChangeFrequency(scale[list[i]]) 
+      if i == 6: 
+         time.sleep(1) 
+      else : 
+         time.sleep(0.5)
+      p.stop() 
+finally: 
+   GPIO.cleanup()
