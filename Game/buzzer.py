34c73439703aab23aabeffe96ba39bc78924d88c
sungyeon Kim <youn010403@naver.com>
2020-11-19 21:47:12 +0900

    add

--- Game/buzzer.py
+++ Game/buzzer.py
@@ -1,10 +1,27 @@
 from gpiozero import Buzzer
-from time import sleep
+import RPi.GPIO as GPIO
+from time
+GPIO.setwarning(False)
+GPIO.setmode(GPIO.BCM)
+
 buzzer = Buzzer(17)
-while True:
-    buzzer.on()
-    sleep(1)
-    buzzer.off()
-    sleep(1)
-while True:
-    buzzer.beep()
+scale=[261,294,329,349,392,440,493,523]
+GPIO.setup(buzzer,GPIO.OUT)
+p=GPIO.PWM(buzzer,600)
+p.start(50)
+try:
+    for i in range(8):
+        p.ChangeFrequency(scale[i])
+        time.sleep(0.5)
+finally:
+    p.stop()
+    GPIO.cleanup()
+    
+#while True:
+    #buzzer.on()
+    #sleep(1)
+    #buzzer.off()
+    #sleep(1)
+#while True:
+    #buzzer.beep()
+
