bdfb1a4c60446c8f14d0f59d420ec29356eb1a45
sungyeon Kim <youn010403@naver.com>
2020-11-19 22:05:30 +0900

    add

--- Game/buzzer.py
+++ Game/buzzer.py
@@ -1,17 +1,26 @@
-from gpiozero import Buzzer
 import RPi.GPIO as GPIO
 import time
-buzzer=Buzzer(17)
-while True:
-    for i in [261,294,329,349]:
-        p=GPIO.PWM(buzzer,i)
-        p.start(50)
-        time.sleep(1)
-        p.stop()
-        #buzzer.on()
-        #time.sleep(1)
-        #buzzer.off()
-        #time.sleep(1)
-while True:
-    buzzer.beep()
 
+list =[261,294,329,349,393,440,493,523]
+num =[2,2,2,2,2,2,2,4,0,1,2,3,3,3,3,3,2,2,2,2,1,1,0,1,4,2,2,2,2,2,2,2,4,0,1,2,3,3,3,3,3,2,2,2,4,4,3,2,0]
+buzzer_pin= 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buzzer_pin,GPIO.OUT)
+
+
+
+try: 
+    pwm= GPIO.PWM(buzzer_pin,100);
+    pwm.start(100)
+    pwm.ChangeDutyCycle(90)
+                    
+    for i in range(80):
+        pwm.ChangeFrequency(list[num[i]])
+        time.sleep(0.3)
+                                                
+except KeyboardInterrupt: 
+        PWM.stop()
+        GPIO.cleanup()
+
+            
