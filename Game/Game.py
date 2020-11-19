3b1bd558cdbca0e79fef0e12a7f98e88552029a4
sungyeon Kim <youn010403@naver.com>
2020-11-20 04:13:13 +0900

    add

--- Game/Game.py
+++ Game/Game.py
@@ -1,18 +1,25 @@
 from match import*
 from original import*
 from gallery import*
+from LED_display import*
 while True:
    print("MODE")
    print("1. Original \n 2. Matching \n 3. Gallery \n 4. Turn off")
    choice=int(input("Select the Mode : "))
    if choice==1:
       memory_key()
+      refresh(screen)   #erase
+      draw_matrix(screen);print() #erase
       continue
    elif choice==2:
       memory_to()
+      refresh(screen)
+      draw_matrix(screen);print()
       continue
    elif choice==3:
       gallery_mode_exe()
+      refresh(screen)
+      draw_matrix(screen);print()
       continue
    elif choice==4:
       break
