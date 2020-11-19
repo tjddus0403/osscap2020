fcc8e4c4f48021028b37736a1031c90347fa33f9
tjddus0403 <70639589+tjddus0403@users.noreply.github.com>
2020-11-19 20:45:39 +0900

    Update 부저 확인.py

--- Game/부저 확인.py
+++ Game/부저 확인.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO 
-import time GPIO.setmode(GPIO.BCM) 
+import time 
+GPIO.setmode(GPIO.BCM) 
 gpio_pin = 12 
 scale = [ 261, 294, 329, 349, 392, 440, 493, 523 ] 
 GPIO.setup(gpio_pin, GPIO.OUT) 
