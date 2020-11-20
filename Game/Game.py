8a8b775e1e154eef8d8de7d0ec28c3a3b826da69
tjddus0403 <70639589+tjddus0403@users.noreply.github.com>
2020-11-20 20:03:34 +0900

    Update Game.py

--- Game/Game.py
+++ Game/Game.py
@@ -8,18 +8,12 @@ while True:
    choice=int(input("Select the Mode : "))
    if choice==1:
       memory_key()
-      refresh()   #erase
-      draw_matrix();print() #erase
       continue
    elif choice==2:
       memory_to()
-      refresh(screen)
-      draw_matrix(screen);print()
       continue
    elif choice==3:
       gallery_mode_exe()
-      refresh(screen)
-      draw_matrix(screen);print()
       continue
    elif choice==4:
       break
