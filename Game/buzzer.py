426ee9ff7903d61dbac62825cdbbf228977fdc2b
tjddus0403 <70639589+tjddus0403@users.noreply.github.com>
2020-11-21 02:09:29 +0900

    Update buzzer.py

--- Game/buzzer.py
+++ Game/buzzer.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 import time
-
-list=[261.6256,293.6648,329.6276,349.2282,391.9954,440,493.8833,523.2511]
-num=[0,1,2,3,4,5,6,7]
+       #0       #1            #2      #3       #4       #5      #6       #7        #8       #9         #10      #11     #12   #13     
+#list=[261.6256|,293.6648|,329.6276|,349.2282|,391.9954|,440|,493.8833|,523.2511|,587.3295|,659.2551|,698.4565|,783.9909|,880|,987.7666]
+#num=[2,4,5,8,8,7,6,5,6,4,6,8,11,11,11,12,10,9,10,5,]
+haul=[293, 391, 493, 587, 587, 523, 493, 440, 493, 391, 493, 587, 783, 783, 783, 880, 693, 659, 698, 440, 554, 698, 880, 783, 698, 659, 698, 783, 698, 659, 587, 523, 493, 523, 587, 523, 391, 440]
 buzzer_pin=17
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -11,9 +12,12 @@ try:
     pwm=GPIO.PWM(buzzer_pin,100);
     pwm.start(100)
     pwm.ChangeDutyCycle(90)
-    for i in range(len(num)):
+    for i in haul:
+        pwm.ChangeFrequency(i)
+        time.sleep(0.5)
+   ''' for i in range(len(num)):
         pwm.ChangeFrequency(list[num[i]])
-        time.sleep(1)
+        time.sleep(1)'''
 except KeyboardInterrupt:
     pwm.stop()
     GPIO.cleanup()
