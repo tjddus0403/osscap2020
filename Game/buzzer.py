dfad59aa7983a903dea6550dab194186d234b0c0
tjddus0403 <70639589+tjddus0403@users.noreply.github.com>
2020-11-20 22:22:28 +0900

    Update buzzer.py

--- Game/buzzer.py
+++ Game/buzzer.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-list=[261,294,329,349,393]
-num=[1,2,3,4]
+list=[261,294,329,349,393,440,493,523]
+num=[0,1,2,3,4,5,6,7]
 buzzer_pin=17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buzzer_pin,GPIO.OUT)
