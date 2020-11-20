0ba6cb16bcea93d1c6cc1f1f2eedb2ad76f736e4
tjddus0403 <70639589+tjddus0403@users.noreply.github.com>
2020-11-21 01:46:47 +0900

    Update Game.py

--- Game/Game.py
+++ Game/Game.py
@@ -7,17 +7,19 @@ while True:
    print("1. Original \n 2. Matching \n 3. Gallery \n 4. Turn off")
    choice=int(input("Select the Mode : "))
    if choice==1:
-      GPIO.setmode(GPIO.BCM)
       memory_key()
-      GPIO.cleanup()
-      continue
+      break
+      #continue
    elif choice==2:
       memory_to()
-      continue
+      break
+      #continue
    elif choice==3:
       gallery_mode_exe()
-      continue
+      break
+      #continue
    elif choice==4:
       break
    else:
+      print("잘못된 번호를 입력하셨습니다.")
       continue
