541c2ca17cb7ce2cef2851cc27f1524b7dd6aa92
tjddus0403 <70639589+tjddus0403@users.noreply.github.com>
2020-11-23 18:58:54 +0900

    Update match.py

--- Game/match.py
+++ Game/match.py
@@ -228,6 +228,7 @@ def memory_to():
       
       if i==0:
          runtime=(end_time-start_time)//1
+         runtime=int(runtime)
          f = open("짝맞추기1등.txt", 'r')
          file = f.read()
          f.close()
