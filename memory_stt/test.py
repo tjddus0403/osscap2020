6020db00f580dd99efb6d62bb6b5092449e21f19
shimyoon <seokyung1115@gmail.com>
2020-11-23 13:08:54 +0900

    backup

---
+++ memory_stt/test.py
@@ -0,0 +1,36 @@
+import gspeech
+import time
+
+def voice():
+  gsp = gspeech.Gspeech()
+  while Ture:
+      stt = gsp.getText()
+      if stt is None:
+          break
+      print(stt)
+      time.sleep(0.01)
+      if ('좌' in stt):
+        return 'a'
+      elif ('우' in stt):
+        return 'd'
+      elif ('하' in stt):
+        return 's'
+      elif ('끝' in stt):
+        return ' '
+      elif ('빨강' in stt):
+        return 'r'
+      elif ('노랑' in stt):
+        return 'o'
+      elif ('초록' in stt):
+        return 'g'
+      elif ('지워' in stt):
+        return 'e'
+      elif ('열기' in stt):
+        return 'o'
+      elif ('힌트' in stt):
+        return 'h'
+
+key=voice()
+print(key)
+
+
