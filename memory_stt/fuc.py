962b0e9facfd4fc82bbe29cdf3be89848ca85c80
shimyoon <seokyung1115@gmail.com>
2020-11-25 15:13:26 +0900

    fuc.py added

---
+++ memory_stt/fuc.py
@@ -0,0 +1,44 @@
+import gspeech2
+import time
+
+import copy
+
+def voice():
+  gsp = gspeech2.Gspeech()
+  while Ture:
+      stt = gsp.getText()
+      if stt is None:
+         break
+      print(stt)
+      time.sleep(0.01)
+      if ('좌' in stt):
+        return 'a'
+        break
+      if ('우' in stt):
+        return 'd'
+        break
+      if ('하' in stt):
+        return 's'
+        break
+      if ('끝' in stt):
+        return ' '
+        break
+      if ('빨강' in stt):
+        return 'r'
+        break
+      if ('노랑' in stt):
+        return 'o'
+        break
+      if ('초록' in stt):
+        return 'g'
+        break
+      if ('지워' in stt):
+        return 'e'
+        break
+      if ('열기' in stt):
+        return 'o'
+        break
+      if ('힌트' in stt):
+        return 'h'
+        break
+
