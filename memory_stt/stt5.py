719d9f5ac788e2b3e0238012356d97ac4447fd14
shimyoon <seokyung1115@gmail.com>
2020-11-25 15:19:09 +0900

    stt5.py edit

--- memory_stt/stt5.py
+++ memory_stt/stt5.py
@@ -4,47 +4,7 @@ import copy
 from matrix import*
 from random import*
 
-import gspeech2
-import time
-
-def voice():
-  gsp = gspeech2.Gspeech()
-  while Ture:
-      stt = gsp.getText()
-      if stt is None:
-         break
-      print(stt)
-      time.sleep(0.01)
-      if ('좌' in stt):
-        return 'a'
-        break
-      if ('우' in stt):
-        return 'd'
-        break
-      if ('하' in stt):
-        return 's'
-        break
-      if ('끝' in stt):
-        return ' '
-        break
-      if ('빨강' in stt):
-        return 'r'
-        break
-      if ('노랑' in stt):
-        return 'o'
-        break
-      if ('초록' in stt):
-        return 'g'
-        break
-      if ('지워' in stt):
-        return 'e'
-        break
-      if ('열기' in stt):
-        return 'o'
-        break
-      if ('힌트' in stt):
-        return 'h'
-        break
+import fuc
 
 
 def draw_matrix(m):
@@ -212,7 +172,7 @@ while True:
          print('Fix the color block : r(red), y(yellow), g(green)')
          print('Erase the block : e(erase)')
          print('Finish : \' \'')
-         key=voice()
+         key=fuc.voice()
          if key=='q':
             print('Game terminated')
             break
