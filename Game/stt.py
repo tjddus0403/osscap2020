5b162cf6656e8ba931a7f9c95c110ded5435f696
shimyoon <seokyung1115@gmail.com>
2020-11-25 15:46:09 +0900

    edit by sy

--- Game/stt.py
+++ Game/stt.py
@@ -5,36 +5,7 @@ import LED_display as LMD
 import threading
 from score import*
 import gspeech2
-
-def voice():
-  gsp = gspeech2.Gspeech()
-  while True:
-      stt = gsp.getText()
-      if stt is None:
-          break
-      time.sleep(0.01)
-      if ('좌' in stt):
-        return 'a'
-      elif ('우' in stt):
-        return 'd'
-      elif ('하' in stt):
-        return 's'
-      elif ('상' in stt):
-        return 'w'
-      elif ('끝' in stt):
-        return ' '
-      elif ('빨강' in stt):
-        return 'r'
-      elif ('노랑' in stt):
-        return 'y'
-      elif ('초록' in stt):
-        return 'g'
-      elif ('지워' in stt):
-        return 'e'
-      elif ('열기' in stt):
-        return 'o'
-      elif ('힌트' in stt):
-        return 'h'
+import fuc
 
 
 def LED_init():
@@ -214,7 +185,7 @@ def memory_key():
                print('Erase the block : e(erase)')
                print('Hint : h(hint)')
                print('Finish : \' \'')
-               key= voice()
+               key = fuc.voice()
                if key=='q':
                   print('Game terminated')
                   break
