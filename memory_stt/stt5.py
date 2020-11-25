1542cdab14bac8f51e7d2f6a47d636e17581477f
KS <70687128+shimyoon@users.noreply.github.com>
2020-11-25 15:06:16 +0900

    Update stt5.py

--- memory_stt/stt5.py
+++ memory_stt/stt5.py
@@ -4,35 +4,47 @@ import copy
 from matrix import*
 from random import*
 
+import gspeech2
+import time
+
 def voice():
   gsp = gspeech2.Gspeech()
-  while True:
+  while Ture:
       stt = gsp.getText()
       if stt is None:
-          break
+         break
+      print(stt)
       time.sleep(0.01)
       if ('좌' in stt):
         return 'a'
-      elif ('우' in stt):
+        break
+      if ('우' in stt):
         return 'd'
-      elif ('하' in stt):
+        break
+      if ('하' in stt):
         return 's'
-      elif ('상' in stt):
-        return 'w'
-      elif ('끝' in stt):
+        break
+      if ('끝' in stt):
         return ' '
-      elif ('빨강' in stt):
+        break
+      if ('빨강' in stt):
         return 'r'
-      elif ('노랑' in stt):
-        return 'y'
-      elif ('초록' in stt):
-        return 'g'
-      elif ('지워' in stt):
-        return 'e'
-      elif ('열기' in stt):
+        break
+      if ('노랑' in stt):
         return 'o'
-      elif ('힌트' in stt):
+        break
+      if ('초록' in stt):
+        return 'g'
+        break
+      if ('지워' in stt):
+        return 'e'
+        break
+      if ('열기' in stt):
+        return 'o'
+        break
+      if ('힌트' in stt):
         return 'h'
+        break
 
 
 def draw_matrix(m):
