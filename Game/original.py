bfc5c17c7b147c51c4327b650ed8904d7d2c3ac0
tjddus0403 <70639589+tjddus0403@users.noreply.github.com>
2020-11-29 15:12:48 +0900

    Update original.py

--- Game/original.py
+++ Game/original.py
@@ -310,6 +310,6 @@ def memory_key(): #기억력 게임 original ver 게임 내용을 담은 함수
                            print("새로운 1등 기록 : ",success)
                            f.close()
                    elif float(line)==success:
-                        printf("축하드립니다. 공동 1등 입니다!")
+                        print("축하드립니다. 공동 1등 입니다!")
                break
       
